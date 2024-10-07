b4fd7068df14d464d5a0cc9a83f20bb71e78e71c
DvlPrSujalPatel <sujal7455@gmail.com>
2024-10-07 20:58:25 +0530

    more animations added to make it smooth

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-
 let imgBox = document.getElementById("imgBox")
 let qrImage = document.getElementById("qrImage")
 let qrText = document.getElementById("qrText")
 
 function QrGenerator(){
+    const button = document.querySelector('.container button');
+    button.classList.add('active');
+    setTimeout(() => {
+        button.classList.remove('active');
+    }, 200);
+
     if(qrText.value.length > 0){
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
         imgBox.classList.add("show-img")
